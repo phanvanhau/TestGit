86170c16209d13fad82c71460607e82de7d7940f
phanvanhau <johnnyhau1207@gmail>
2014-06-29 12:12:17 +0200

    ajout de deux premiers fichiers à tester

---
+++ SignedOutServlet.java
@@ -0,0 +1,26 @@
+package guestbook;
+
+import java.io.IOException;
+
+import javax.servlet.http.*;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class SignedOutServlet extends HttpServlet {
+	 @Override
+	    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	              throws IOException {
+	        UserService userService = UserServiceFactory.getUserService();
+	        User user = userService.getCurrentUser();
+
+	        if (user != null) {
+	        	 resp.getWriter().println("Vous êtes bien déconnecté");
+	        	resp.sendRedirect(userService.createLogoutURL(req.getRequestURI()));
+	        	 
+	        } 
+	        else
+	        	  resp.getWriter().println("Vous n'êtes pas encore connecté");
+	    }
+}
